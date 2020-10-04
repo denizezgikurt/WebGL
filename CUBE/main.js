634246258e3a86926b33d6d2f4de2603a09b34a4
Deniz Ezgi Kurt <denizezgikurt@gmail.com>
2020-10-04 14:21:39 +0200

    add init function and move the scene, camera and renderer inside the init function (also removed const declarations and called them globally)

--- CUBE/main.js
+++ CUBE/main.js
@@ -1,25 +1,31 @@
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-const camera = new THREE.PerspectiveCamera(
-	75, //field of view (FOV) the extent of the scene that is seen on the display at any given moment. The value is in degrees.
-	window.innerWidth / window.innerHeight, // Aspect Ratio -  use the width of the element divided by the height, or you'll get the same result as when you play old movies on a widescreen TV - the image looks squished.
-	0.1, // near clipping plane
-	1000 // far clipping plane
-);
+function init() {
 
-const renderer = new THREE.WebGLRenderer({antialias: true });
+	scene = new THREE.Scene();
 
-//init renderer - render the scene with the camera
-renderer.setSize(window.innerWidth, window.innerHeight);
+	camera = new THREE.PerspectiveCamera(
+		75, //field of view (FOV) the extent of the scene that is seen on the display at any given moment. The value is in degrees.
+		window.innerWidth / window.innerHeight, // Aspect Ratio -  use the width of the element divided by the height, or you'll get the same result as when you play old movies on a widescreen TV - the image looks squished.
+		0.1, // near clipping plane
+		1000 // far clipping plane
+	);
 
-document.body.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer({antialias: true });
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( { color: 0xfd855a } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	//init renderer - render the scene with the camera
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.z = 5;
+	document.body.appendChild( renderer.domElement );
+
+	const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+	const material = new THREE.MeshBasicMaterial( { color: 0xfd855a } );
+	cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+};
 
 function animate() {
 	requestAnimationFrame(animate);
@@ -30,4 +36,5 @@ function animate() {
 	renderer.render(scene, camera);
 }
 
+init();
 animate();
