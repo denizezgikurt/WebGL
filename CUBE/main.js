50860d3b13235184413933be36658efa60c805b4
Deniz Ezgi Kurt <denizezgikurt@gmail.com>
2020-10-04 14:17:51 +0200

    Project Cube - set up WEbGL scene, camera and renderer, with the dimensions of the cube

---
+++ CUBE/main.js
@@ -0,0 +1,22 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+	75, //field of view (FOV) the extent of the scene that is seen on the display at any given moment. The value is in degrees.
+	window.innerWidth / window.innerHeight, // Aspect Ratio -  use the width of the element divided by the height, or you'll get the same result as when you play old movies on a widescreen TV - the image looks squished.
+	0.1, // near clipping plane
+	1000 // far clipping plane
+);
+
+const renderer = new THREE.WebGLRenderer({antialias: true });
+
+//init renderer - render the scene with the camera
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( { color: 0xfd855a } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
